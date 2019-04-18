2dc6a704421274faa8c7ba9f33137effae88a8e3
nengwen.chen <nengwen.chen@amlogic.com>
2019-04-18 14:38:19 +0800

    hardware_dmx: Remove unused code can cause memory corruption [1/1]
    PD#SWPL-7378
    
    Problem:
    Removing unused code can cause memory corruption.
    
    Solution:
    Removing unused code can cause memory corruption.
    
    Verify:
    verified by x301
    
    Change-Id: Ie50472998247a19337c5bacd16606a0bfa45aef0

--- drivers/stream_input/parser/hw_demux/aml_dvb.c
+++ drivers/stream_input/parser/hw_demux/aml_dvb.c
@@ -2215,7 +2215,6 @@ static int aml_dvb_probe(struct platform_device *pdev)
 		struct device_node *node_tuner = NULL;
 		struct device_node *node_i2c = NULL;
 		u32 i2c_addr = 0xFFFFFFFF;
-		struct tuner_config *cfg = NULL;
 		u32 value = 0;
 		int j = 0;
 
@@ -2238,7 +2237,6 @@ static int aml_dvb_probe(struct platform_device *pdev)
 					s_demod_type[i] = DEMOD_INTERNAL;
 				}
 
-				memset(&cfg, 0, sizeof(struct tuner_config));
 				memset(buf, 0, 32);
 				snprintf(buf, sizeof(buf), "fe%d_tuner",i);
 				node_tuner = of_parse_phandle(pdev->dev.of_node, buf, 0);
