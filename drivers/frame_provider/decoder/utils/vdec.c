d4d06b645029917ab1bb0474179f67c9006ddc29
Nanxin Qin <nanxin.qin@amlogic.com>
2019-06-04 17:06:46 +0800

    the share sources need to be protected by lock.
    PD#OTT-4411
    
    Change-Id: I01bee0c10bb1263722f12893484dce819c26bc71
    Signed-off-by: Nanxin Qin <nanxin.qin@amlogic.com>

--- drivers/frame_provider/decoder/utils/vdec.c
+++ drivers/frame_provider/decoder/utils/vdec.c
@@ -4242,6 +4242,7 @@ static ssize_t dump_vdec_blocks_show(struct class *class,
 {
 	struct vdec_core_s *core = vdec_core;
 	char *pbuf = buf;
+	unsigned long flags = vdec_core_lock(vdec_core);
 
 	if (list_empty(&core->connected_vdec_list))
 		pbuf += sprintf(pbuf, "connected vdec list empty\n");
@@ -4252,6 +4253,7 @@ static ssize_t dump_vdec_blocks_show(struct class *class,
 				pbuf, PAGE_SIZE - (pbuf - buf));
 		}
 	}
+	vdec_core_unlock(vdec_core, flags);
 
 	return pbuf - buf;
 }
@@ -4260,6 +4262,7 @@ static ssize_t dump_vdec_chunks_show(struct class *class,
 {
 	struct vdec_core_s *core = vdec_core;
 	char *pbuf = buf;
+	unsigned long flags = vdec_core_lock(vdec_core);
 
 	if (list_empty(&core->connected_vdec_list))
 		pbuf += sprintf(pbuf, "connected vdec list empty\n");
@@ -4270,6 +4273,7 @@ static ssize_t dump_vdec_chunks_show(struct class *class,
 				pbuf, PAGE_SIZE - (pbuf - buf));
 		}
 	}
+	vdec_core_unlock(vdec_core, flags);
 
 	return pbuf - buf;
 }
