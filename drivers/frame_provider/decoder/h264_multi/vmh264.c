9f4f222e2c186deb8284ba879338685920c5a83b
Hui Zhang <hui.zhang@amlogic.com>
2019-07-08 14:56:29 +0800

    vmh264: reset bufmgr when ref error happaned [1/1]
    PD#SWPL-10897
    
    Problem:
    hot-plug dvb cable cause video decoder error and not
    frame output
    
    Solution:
    enable bufmgr reset  when reference error happend
    
    Verify:
    x301
    
    Change-Id: I9cf1e39e5e3b1bf1f8f7de78877fda8f3a294b11
    Signed-off-by: Hui Zhang <hui.zhang@amlogic.com>

--- drivers/frame_provider/decoder/h264_multi/vmh264.c
+++ drivers/frame_provider/decoder/h264_multi/vmh264.c
@@ -265,7 +265,7 @@ static unsigned int i_only_flag;
 	bit[12] i_only when error happen
 	bit[13] 0: mark error according to last pic, 1: ignore mark error
 */
-static unsigned int error_proc_policy = 0xf36; /*0x1f14*/
+static unsigned int error_proc_policy = 0xfb6; /*0x1f14*/
 
 
 /*
