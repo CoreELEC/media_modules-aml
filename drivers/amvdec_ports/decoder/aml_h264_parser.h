2bedd007f4caf37c0e83751c152ef891445dcd78
Gan Zhang <gan.zhang@amlogic.com>
2019-07-29 16:26:15 +0800

    media_module: clean no license source file [1/1]
    PD#SWPL-11720
    
    Problem:
    clean no license source file.
    
    Solution:
    Add license information to the source file.
    
    Verify:
    U212
    
    Change-Id: I928f2dd834b98bba6f3722d1146887896a1c0e49
    Signed-off-by: Gan Zhang <gan.zhang@amlogic.com>

--- drivers/amvdec_ports/decoder/aml_h264_parser.h
+++ drivers/amvdec_ports/decoder/aml_h264_parser.h
@@ -1,3 +1,21 @@
+/*
+ * drivers/amvdec_ports/decoder/aml_h264_parser.h
+ *
+ * Copyright (C) 2015 Amlogic, Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+
+
 #ifndef AML_H264_PARSER_H
 #define AML_H264_PARSER_H
 
