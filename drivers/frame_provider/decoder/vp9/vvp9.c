f63d2c1ed3dd863cac772e58ca3d06ea785958d4
Arthur Liberman <arthur_liberman@hotmail.com>
2019-11-23 14:57:34 +0200

    decoders: automatically switch on double write 1:2 mode with 8K content

--- drivers/frame_provider/decoder/vp9/vvp9.c
+++ drivers/frame_provider/decoder/vp9/vvp9.c
@@ -171,11 +171,12 @@ static u32 mv_buf_margin;
  *	3, (1/4):(1/4) ratio, with both compressed frame included
  *	4, (1/2):(1/2) ratio;
  *	0x10, double write only
- *	0x100, if > 1080p,use mode 4,else use mode 1;
- *	0x200, if > 1080p,use mode 2,else use mode 1;
- *	0x300, if > 720p, use mode 4, else use mode 1;
+ *	0x100, if > 1080p, use mode 4, else use mode 1;
+ *	0x200, if > 1080p, use mode 2, else use mode 1;
+ *	0x300, if > 720p,  use mode 4, else use mode 1;
+ *	0x900, if > 2160p, use mode 4, else use mode 0;
  */
-static u32 double_write_mode;
+static u32 double_write_mode = 0x900;
 
 #define DRIVER_NAME "amvdec_vp9"
 #define MODULE_NAME "amvdec_vp9"
@@ -1624,6 +1625,12 @@ static int get_double_write_mode(struct VP9Decoder_s *pbi)
 			if (w > 1280 && h > 720)
 				dw = 0x4; /*1:2*/
 			break;
+		case 0x900:
+			if (w > 3840 && h > 2176)
+				dw = 0x4; /*1:2*/
+			else
+				dw = 0x0; /*off*/
+			break;
 		default:
 			break;
 		}
@@ -1654,6 +1661,12 @@ static int get_double_write_mode_init(struct VP9Decoder_s *pbi)
 	case 0x300:
 		if (w > 1280 && h > 720)
 			dw = 0x4; /*1:2*/
+		break;
+	case 0x900:
+		if (w > 3840 && h > 2176)
+			dw = 0x4; /*1:2*/
+		else
+			dw = 0x0; /*off*/
 		break;
 	default:
 		dw = valid_dw_mode;
