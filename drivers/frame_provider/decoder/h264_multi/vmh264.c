78ef617638aaaca5ba2db5b3a5945828da408e89
Arthur Liberman <arthur_liberman@hotmail.com>
2019-11-23 19:51:46 +0200

    decoders: force di for interlaced only see 58ec5bce18081018174b30e38338e4cc14174a89 for details

--- drivers/frame_provider/decoder/h264_multi/vmh264.c
+++ drivers/frame_provider/decoder/h264_multi/vmh264.c
@@ -2390,7 +2390,7 @@ static int check_force_interlace(struct vdec_h264_hw_s *hw,
 	}
 
 	if ((dec_control & DEC_CONTROL_FLAG_FORCE_2997_1080P_INTERLACE)
-		&& hw->bitstream_restriction_flag
+		&& !hw->bitstream_restriction_flag
 		&& (hw->frame_width == 1920)
 		&& (hw->frame_height >= 1080)
 		&& (hw->frame_dur == 3203 || hw->frame_dur == 3840)) {
