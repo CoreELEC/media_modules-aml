85d0febe651ef77ea69da923ce9562cbce6bf159
Yixin Peng <yixin.peng@amlogic.com>
2019-12-06 19:36:05 +0800

    media_module: h264 mosaics appear after seek operation [1/1]
    PD#SWPL-17412
    
    Problem:
    reset the iqidct register cause mosaics.
    
    Solution:
    Don't to reset the iqidct register.
    
    Verify:
    AC214
    
    Change-Id: I24c3d8c9b95134f65e1dab02404bb993761467e4
    Signed-off-by: Yixin Peng <yixin.peng@amlogic.com>

--- drivers/frame_provider/decoder/utils/vdec.c
+++ drivers/frame_provider/decoder/utils/vdec.c
@@ -3744,7 +3744,7 @@ void vdec_reset_core(struct vdec_s *vdec)
 	 */
 	if ((get_cpu_major_id() >= AM_MESON_CPU_MAJOR_ID_SM1) &&
 		(get_cpu_major_id() != AM_MESON_CPU_MAJOR_ID_TL1)) {
-		WRITE_VREG(DOS_SW_RESET0, (1<<3)|(1<<4)|(1<<5)|(1<<6)|(1<<7)|(1<<8)|(1<<9));
+		WRITE_VREG(DOS_SW_RESET0, (1<<3)|(1<<4)|(1<<5)|(1<<7)|(1<<8)|(1<<9));
 	} else {
 		WRITE_VREG(DOS_SW_RESET0,
 			(1<<3)|(1<<4)|(1<<5));
