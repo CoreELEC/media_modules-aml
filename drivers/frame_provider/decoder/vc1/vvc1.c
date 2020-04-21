6664bf120c898cf47b7cbe4fe622663534ed7193
shilong.yang <shilong.yang@amlogic.com>
2020-04-21 10:46:33 +0800

    vc1: fixed the crash when some vc1 video ff. [1/1]
    PD#SWPL-24046
    
    Problem:
    fast forward vc1 video, crash happened.
    
    Solution:
    reset vdec status when resume.
    
    Verify:
    TM2
    
    Change-Id: I12b46fcaf11ed17bf52068de8d2b5822b089e1e8
    Signed-off-by: shilong.yang <shilong.yang@amlogic.com>

--- drivers/frame_provider/decoder/vc1/vvc1.c
+++ drivers/frame_provider/decoder/vc1/vvc1.c
@@ -43,7 +43,7 @@
 #include "../utils/firmware.h"
 #include <linux/amlogic/tee.h>
 #include <linux/delay.h>
-
+#include "../../../common/chips/decoder_cpu_ver_info.h"
 
 
 #define DRIVER_NAME "amvdec_vc1"
@@ -1216,6 +1216,9 @@ static int amvdec_vc1_remove(struct platform_device *pdev)
 	}
 
 	amvdec_disable();
+
+	if (get_cpu_major_id() >= AM_MESON_CPU_MAJOR_ID_TM2)
+		vdec_reset_core(NULL);
 
 	if (mm_blk_handle) {
 		decoder_bmmu_box_free(mm_blk_handle);
