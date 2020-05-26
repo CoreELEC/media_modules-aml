f3eded78794916b1916c19658197f7bad05124fb
Nanxin Qin <nanxin.qin@amlogic.com>
2020-05-26 20:44:59 +0800

    vdec: fixed the issue of the swap memory leak. [1/1]
    PD#SWPL-26496
    
    Problem:
    cobalt can't play clear stream after play DRM stream
    
    Solution:
    if the swap memory did not free, then the tvp pool
    hold a mass of memory that caused to clear stream
    playback fail as not enough memory to decoding.
    
    Verify:
    u212
    
    Change-Id: I18fcbe228a72c2d445f47058aed4401365365aec
    Signed-off-by: Nanxin Qin <nanxin.qin@amlogic.com>

--- drivers/frame_provider/decoder/utils/vdec_input.c
+++ drivers/frame_provider/decoder/utils/vdec_input.c
@@ -1116,16 +1116,18 @@ void vdec_input_release(struct vdec_input_s *input)
 	}
 
 	/* release swap pages */
-	if (input->swap_page) {
-		if (vdec_secure(input->vdec))
+	if (vdec_secure(input->vdec)) {
+		if (input->swap_page_phys)
 			codec_mm_free_for_dma("SWAP", input->swap_page_phys);
-		else
+	} else {
+		if (input->swap_page) {
 			dma_free_coherent(v4l_get_dev_from_codec_mm(),
 				PAGE_SIZE, input->swap_page,
-					input->swap_page_phys);
-		input->swap_page = NULL;
-		input->swap_page_phys = 0;
+				input->swap_page_phys);
+		}
 	}
+	input->swap_page = NULL;
+	input->swap_page_phys = 0;
 	input->swap_valid = false;
 }
 EXPORT_SYMBOL(vdec_input_release);
