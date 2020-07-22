7080d9389e1b8005170d6f401e3ab8e4120d873e
Gan Zhang <gan.zhang@amlogic.com>
2020-07-22 20:17:37 +0800

    media_module: fix amvdec_ports.ko loading error [1/1]
    PD#SWPL-29656
    
    Problem:
    fix amvdec_ports.ko loading error.
    
    Solution:
    export the function.
    
    Verify:
    local build
    
    Change-Id: I1df171daa541f468e5cf23f9e6df4d97d0ebb7e2
    Signed-off-by: Gan Zhang <gan.zhang@amlogic.com>

--- drivers/amvdec_ports/decoder/aml_vp9_parser.h
+++ drivers/amvdec_ports/decoder/aml_vp9_parser.h
@@ -174,11 +174,12 @@ struct vp9_param_sets {
 	struct VP9Context ctx;
 };
 
-int vp9_superframe_split_filter(struct vp9_superframe_split *s);
 
 #ifdef CONFIG_AMLOGIC_MEDIA_V4L_SOFTWARE_PARSER
+int vp9_superframe_split_filter(struct vp9_superframe_split *s);
 int vp9_decode_extradata_ps(u8 *data, int size, struct vp9_param_sets *ps);
 #else
+inline int vp9_superframe_split_filter(struct vp9_superframe_split *s) { return -1;}
 inline int vp9_decode_extradata_ps(u8 *data, int size, struct vp9_param_sets *ps) { return -1; }
 #endif
 
