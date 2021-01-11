519d3a5401bb0f0e19079d3a71271c5d386db906
Peng Yixin <yixin.peng@amlogic.com>
2021-01-11 10:12:56 +0800

    media_module: can't get the correct width and height information [1/1]
    PD#SWPL-40808
    
    Problem:
    When the width and height of the file exceed
    the decoding limit, the user layer cannot
    read the width and height information correctly.
    
    Solution:
    Transmit the width and height information from
    the header to solve this issue.
    
    Verify:
    AM311
    
    Change-Id: I036eaa410eeff7391cd571a4e08ee201884e4e83
    Signed-off-by: Peng Yixin <yixin.peng@amlogic.com>

--- drivers/frame_provider/decoder/vp9/vvp9.c
+++ drivers/frame_provider/decoder/vp9/vvp9.c
@@ -1234,6 +1234,8 @@ struct VP9Decoder_s {
 	int buffer_wrap[FRAME_BUFFERS];
 	int last_width;
 	int last_height;
+	u32 error_frame_width;
+	u32 error_frame_height;
 };
 
 static int vp9_print(struct VP9Decoder_s *pbi,
@@ -1353,9 +1355,13 @@ static int setup_frame_size(
 	width = params->p.width;
 	height = params->p.height;
 	if (is_oversize(width, height)) {
+		pbi->error_frame_width = width;
+		pbi->error_frame_height = height;
 		vp9_print(pbi, 0, "%s, Error: Invalid frame size\n", __func__);
 		return -1;
 	}
+	pbi->error_frame_width = 0;
+	pbi->error_frame_height = 0;
 
 	/*vp9_read_frame_size(rb, &width, &height);*/
 	if (print_header_info)
@@ -1480,9 +1486,13 @@ static int setup_frame_size_with_refs(
 	}
 
 	if (is_oversize(width, height)) {
+		pbi->error_frame_width = width;
+		pbi->error_frame_height = height;
 		vp9_print(pbi, 0, "%s, Error: Invalid frame size\n", __func__);
 		return -1;
 	}
+	pbi->error_frame_width = 0;
+	pbi->error_frame_height = 0;
 
 	params->p.width = width;
 	params->p.height = height;
@@ -9271,6 +9281,12 @@ int vvp9_dec_status(struct vdec_s *vdec, struct vdec_info *vstatus)
 
 	vstatus->frame_width = frame_width;
 	vstatus->frame_height = frame_height;
+	if (vp9->error_frame_width &&
+		vp9->error_frame_height) {
+		vstatus->frame_width = vp9->error_frame_width;
+		vstatus->frame_height = vp9->error_frame_height;
+	}
+
 	if (vp9->frame_dur != 0)
 		vstatus->frame_rate = ((96000 * 10 / vp9->frame_dur) % 10) < 5 ?
 				96000 / vp9->frame_dur : (96000 / vp9->frame_dur +1);
