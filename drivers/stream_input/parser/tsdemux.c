68700307ccebf82817276ee63d0cbe5dba1530e2
Chuangcheng Peng <chuangcheng.peng@amlogic.com>
2021-10-29 17:16:55 +0800

    media_module: CB2 rest demux when stop playing [1/1]
    PD#SWPL-62937
    
    Problem:
    Demux cannot receive section when stop playing
    
    Solution:
    Reset demux when stop playing
    
    Verify:
    Self test
    
    Change-Id: I822f3b72c69ed500a258ad9592de96c9900918e7
    Signed-off-by: Chuangcheng Peng <chuangcheng.peng@amlogic.com>

--- drivers/stream_input/parser/tsdemux.c
+++ drivers/stream_input/parser/tsdemux.c
@@ -766,6 +766,9 @@ void tsdemux_release(void)
 	WRITE_PARSER_REG(PARSER_VIDEO_RP, 0);
 #endif
 
+	if (enable_demux_driver())
+		tsdemux_reset();
+
 	/* #if MESON_CPU_TYPE >= MESON_CPU_TYPE_MESON6 */
 	/*TODO clk */
 	/*
