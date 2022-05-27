2a0fb9ce5976e45691039b234b8a266779f5d14d
Portisch <hugo.portisch@yahoo.de>
2022-05-27 10:02:05 +0200

    vc1: modifiy debug info

--- drivers/frame_provider/decoder/vc1/vvc1.c
+++ drivers/frame_provider/decoder/vc1/vvc1.c
@@ -374,7 +374,7 @@ static irqreturn_t vvc1_isr(int irq, void *dev_id)
 		picture_type = (reg >> 3) & 7;
 
 		if (buffer_index >= DECODE_BUFFER_NUM_MAX) {
-			pr_info("fatal error, invalid buffer index.");
+			pr_err("fatal error, invalid buffer index.");
 			return IRQ_HANDLED;
 		}
 
@@ -410,7 +410,7 @@ static irqreturn_t vvc1_isr(int irq, void *dev_id)
 					frm.end_pts = pts;
 					frm.rate = (frm.end_pts -
 						frm.start_pts) / frm.num;
-					pr_info("frate before=%d,%d,num=%d\n",
+					pr_debug("frate before=%d,%d,num=%d\n",
 					frm.rate,
 					DUR2PTS(vvc1_amstream_dec_info.rate),
 					frm.num);
@@ -434,7 +434,7 @@ static irqreturn_t vvc1_isr(int irq, void *dev_id)
 						vvc1_amstream_dec_info.rate),
 						RATE_30_FPS,
 						RATE_CORRECTION_THRESHOLD))) {
-						pr_info(
+						pr_debug(
 						"vvc1: frate from %d to %d\n",
 						vvc1_amstream_dec_info.rate,
 						PTS2DUR(frm.rate));
@@ -470,7 +470,7 @@ static irqreturn_t vvc1_isr(int irq, void *dev_id)
 
 		if (reg & INTERLACE_FLAG) {	/* interlace */
 			if (kfifo_get(&newframe_q, &vf) == 0) {
-				pr_info
+				pr_err
 				("fatal error, no available buffer slot.");
 				return IRQ_HANDLED;
 			}
