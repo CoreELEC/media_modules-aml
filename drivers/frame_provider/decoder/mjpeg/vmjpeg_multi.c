674ed9677c4c01dad9791395a5724df8fbedb1a6
miaohong chen <miaohong.chen@amlogic.com>
2022-10-09 19:13:53 +0800

    mjpeg: CB2 some mjpeg stream playback timeout [1/1]
    PD#SWPL-96740
    PD#SWPL-95707
    PD#SWPL-95356
    
    Problem:
    some mjpeg stream playback timeout.
    
    Solution:
    1.add mjpeg frame head when send es data to decoder.
    2.Adjust vdec hardware startup timing
    
    Verify:
    ohm
    
    Change-Id: Iee7d1df2c8544c98a669be055295eeafc6b61ca6
    Signed-off-by: miaohong chen <miaohong.chen@amlogic.com>

--- drivers/frame_provider/decoder/mjpeg/vmjpeg_multi.c
+++ drivers/frame_provider/decoder/mjpeg/vmjpeg_multi.c
@@ -1171,8 +1171,8 @@ static void run(struct vdec_s *vdec, unsigned long mask,
 	start_process_time(hw);
 	hw->last_vld_level = 0;
 	mod_timer(&hw->check_timer, jiffies + CHECK_INTERVAL);
-	vdec_enable_input(vdec);
 	amvdec_start();
+	vdec_enable_input(vdec);
 	hw->stat |= STAT_VDEC_RUN;
 	hw->init_flag = 1;
 
