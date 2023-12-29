bd4d8a1b19fd26f067037b1d2289ec7742c29865
shihong.zheng <shihong.zheng@amlogic.com>
2023-12-29 16:00:47 +0800

    s6: CF1 decoder bringup. [1/1]
    PD#SWPL-150150
    
    Problem:
    s6 decoder bringup.
    
    Solution:
    add s6 decoder features code.
    
    Verify:
    S905X5
    
    Change-Id: Ic03e29669eae14b1fd531c29bdfd14f5dbeb8026
    Signed-off-by: shihong.zheng <shihong.zheng@amlogic.com>
    Signed-off-by: Luan Yuan <luan.yuan@amlogic.com>

--- drivers/frame_provider/decoder/utils/vdec.c
+++ drivers/frame_provider/decoder/utils/vdec.c
@@ -85,10 +85,6 @@
 #include "../../../amvdec_ports/aml_vcodec_drv.h"
 #include "../../../common/media_utils/media_utils.h"
 
-#if 0
-#define PXP_DEBUG
-#endif
-
 #ifdef CONFIG_AMLOGIC_IONVIDEO
 #include <linux/amlogic/media/video_sink/ionvideo_ext.h>
 #else
@@ -973,7 +969,8 @@ static void dec_dmc_port_ctrl(bool dmc_on, u32 target)
 
 	if (target == VDEC_INPUT_TARGET_VLD) {
 		if ((cpu_type == AM_MESON_CPU_MAJOR_ID_S7) ||
-			(cpu_type == AM_MESON_CPU_MAJOR_ID_S7D)) {
+			(cpu_type == AM_MESON_CPU_MAJOR_ID_S7D) ||
+			(cpu_type == AM_MESON_CPU_MAJOR_ID_S6)) {
 			mask = (1 << 8);
 		} else {
 			mask = (1 << 13);	/*bit13: DOS VDEC interface*/
@@ -984,6 +981,8 @@ static void dec_dmc_port_ctrl(bool dmc_on, u32 target)
 		if ((cpu_type == AM_MESON_CPU_MAJOR_ID_S7) ||
 			(cpu_type == AM_MESON_CPU_MAJOR_ID_S7D)) {
 			mask = (1 << 7);
+		} else if  (cpu_type == AM_MESON_CPU_MAJOR_ID_S6) {
+			mask = (1 << 6) | (1 << 7);
 		} else {
 			mask = (1 << 4); /*hevc*/
 			if ((cpu_type >= AM_MESON_CPU_MAJOR_ID_G12A) &&
@@ -1043,6 +1042,9 @@ static void dec_dmc_port_ctrl(bool dmc_on, u32 target)
 			break;
 		case AM_MESON_CPU_MAJOR_ID_S7D:
 			sts_reg_addr = 0xcf;
+			break;
+		case AM_MESON_CPU_MAJOR_ID_S6:
+			sts_reg_addr = 0xd8;
 			break;
 		default:
 			sts_reg_addr = DMC_CHAN_STS;
@@ -1140,37 +1142,15 @@ static void vdec_enable_DMC(struct vdec_s *vdec)
 
 static int vdec_get_hw_type(int value)
 {
-	int type;
-	switch (value) {
-		case VFORMAT_HEVC:
-		case VFORMAT_VP9:
-		case VFORMAT_AVS2:
-		case VFORMAT_AV1:
-		case VFORMAT_AVS3:
-			type = CORE_MASK_HEVC;
-		break;
-
-		case VFORMAT_MPEG12:
-		case VFORMAT_MPEG4:
-		case VFORMAT_H264:
-		case VFORMAT_MJPEG:
-		case VFORMAT_REAL:
-		case VFORMAT_JPEG:
-		case VFORMAT_VC1:
-		case VFORMAT_AVS:
-		case VFORMAT_YUV:
-		case VFORMAT_H264MVC:
-		case VFORMAT_H264_4K2K:
-		case VFORMAT_H264_ENC:
-		case VFORMAT_JPEG_ENC:
-			type = CORE_MASK_VDEC_1;
-		break;
-
-		default:
-			type = -1;
-	}
-
-	return type;
+	if (is_core_hevc_fmt(value))
+		return CORE_MASK_HEVC;
+	else if (is_core_vdec_fmt(value))
+		return CORE_MASK_VDEC_1;
+	else if (value == VFORMAT_H264_ENC ||
+		value == VFORMAT_JPEG_ENC)
+		return CORE_MASK_HCODEC;
+	else
+		return -1;
 }
 
 
@@ -1424,6 +1404,7 @@ static const char * const vdec_device_name[] = {
 	"amvdec_avs2",       "ammvdec_avs2",
 	"amvdec_av1",        "ammvdec_av1",
 	"amvdec_avs3",       "ammvdec_avs3",
+	"amvdec_h266",       "ammvdec_h266",
 };
 
 
@@ -1446,8 +1427,9 @@ static const char * const vdec_device_name[] = {
 	"jpegenc",
 	"amvdec_vp9",
 	"amvdec_avs2",
-	"amvdec_av1"
-	"amvdec_avs3"
+	"amvdec_av1",
+	"amvdec_avs3",
+	"amvdec_h266",
 };
 
 #endif
@@ -3178,12 +3160,7 @@ s32 vdec_init(struct vdec_s *vdec, int is_4k, bool is_v4l)
 	mutex_unlock(&vdec_mutex);
 
 	vdec_input_set_type(&vdec->input, vdec->type,
-			(vdec->format == VFORMAT_HEVC ||
-			vdec->format == VFORMAT_AVS2 ||
-			vdec->format == VFORMAT_VP9 ||
-			vdec->format == VFORMAT_AV1 ||
-			vdec->format == VFORMAT_AVS3
-			) ?
+			(is_core_hevc_fmt(vdec->format)) ?
 				VDEC_INPUT_TARGET_HEVC :
 				VDEC_INPUT_TARGET_VLD);
 	if (vdec_single(vdec) ||
@@ -3593,7 +3570,7 @@ s32 vdec_init(struct vdec_s *vdec, int is_4k, bool is_v4l)
 		vfm_map_remove("dvblpath");
 		vfm_map_add("dvblpath", vdec->vfm_map_chain);
 	}
-
+#ifndef PXP_DEBUG
 	if (!vdec_single(vdec) && !vdec->disable_vfm) {
 		vf_reg_provider(&p->vframe_provider);
 
@@ -3619,6 +3596,7 @@ s32 vdec_init(struct vdec_s *vdec, int is_4k, bool is_v4l)
 			}
 		}
 	}
+#endif
 	if (vdec_single(vdec) && !vdec_secure(vdec)) {
 		if (!is_support_no_parser())
 			tee_config_device_state(DMC_DEV_ID_PARSER, 0);
@@ -3805,6 +3783,7 @@ void vdec_release(struct vdec_s *vdec)
 	vdec_frame_rate_uevent(0);
 	vdec_disconnect(vdec);
 
+#ifndef PXP_DEBUG
 	if (!vdec->disable_vfm && vdec->vframe_provider.name) {
 		if (!vdec_single(vdec)) {
 			if (vdec_core->hint_fr_vdec == vdec
@@ -3817,7 +3796,7 @@ void vdec_release(struct vdec_s *vdec)
 		}
 		vf_unreg_provider(&vdec->vframe_provider);
 	}
-
+#endif
 	if (vdec_core->vfm_vdec == vdec)
 		vdec_core->vfm_vdec = NULL;
 
