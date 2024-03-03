4c2b6867e3eecc4d8d011e3f8c3ad1eef1f9b517
xiaoya.lin <xiaoya.lin@amlogic.com>
2024-03-03 15:25:26 +0800

    encoder: CF1 Optimize insmod ko for txhd2 [1/1]
    PD#SWPL-158715
    
    Problem:
    kernel5.4+media_modules5.15 compile err
    
    Solution:
    Modify driver init
    
    Verify:
    Local
    
    Signed-off-by: xiaoya.lin <xiaoya.lin@amlogic.com>
    Change-Id: I4fcd68174d3ef16046f4f8ed2c8e3cad8bb436c7

--- drivers/frame_sink/encoder/jpeg/jpegenc.c
+++ drivers/frame_sink/encoder/jpeg/jpegenc.c
@@ -5179,7 +5179,7 @@ static struct codec_profile_t jpegenc_profile = {
 
 static s32 __init jpegenc_driver_init_module(void)
 {
-    if (get_cpu_type() == MESON_CPU_MAJOR_ID_TXHD2) {
+    if (get_cpu_major_id() == AM_MESON_CPU_MAJOR_ID_TXHD2) {
         jenc_pr(LOG_DEBUG, "The chip is not support jpegenc!!\n");
         return -1;
     }
--- drivers/frame_sink/encoder/multi/vpu_multi.c
+++ drivers/frame_sink/encoder/multi/vpu_multi.c
@@ -3484,7 +3484,7 @@ static s32 __init vpu_init(void)
 {
 	s32 res;
 
-	if (get_cpu_type() == MESON_CPU_MAJOR_ID_TXHD2) {
+	if (get_cpu_major_id() == AM_MESON_CPU_MAJOR_ID_TXHD2) {
 		enc_pr(LOG_DEBUG, "The chip is not support multi encoder!!\n");
 		return -1;
 	}
