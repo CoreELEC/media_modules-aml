4eb0758363b172da1faed6bfb57769326ea244a6
lele xiang <lele.xiang@amlogic.com>
2024-06-05 06:18:29 +0000

    h265: CB0 The h265 yuv memory exceeds the alloced size. [1/1]
    PD#SWPL-171720
    
    Problem:
    The afbc header and yuv are alloced together, but the afbc header
    does not considering 64k alignment, causing the yuv mem Exceeds
    the appropriate range when yuv start addr with 64k alignment
    
    Solution:
    Do 64kb alignment for the size of the afbc header
    
    Verify:
    T5M
    
    Change-Id: Ic03bacb61f24304d02b39746dedd574742268032
    Signed-off-by: lele xiang <lele.xiang@amlogic.com>

--- drivers/frame_provider/decoder/h265/vh265.c
+++ drivers/frame_provider/decoder/h265/vh265.c
@@ -3622,9 +3622,10 @@ static int cal_current_buf_size(struct hevc_state_s *hevc,
 	int tw_mode = get_triple_write_mode(hevc);
 #endif
 
-	if (hevc->mmu_enable)
+	if (hevc->mmu_enable) {
 		buf_size = hevc_get_header_size(hevc->pic_w, hevc->pic_h);
-	else
+		buf_size = ((buf_size + 0xffff) >> 16) << 16;
+	} else
 		buf_size = 0;
 #ifdef H265_10B_MMU_DW
 	if (hevc->dw_mmu_enable) {
