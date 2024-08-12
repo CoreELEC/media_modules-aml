4444e4e82d86bc691fe48c468a0554076dd1604c
yu.xie <yu.xie@amlogic.com>
2024-08-12 09:19:16 +0000

    v4l: CB1 Support set lowlatency mode in VF. [1/1]
    PD#SWPL-181664
    
    Problem:
    Need support set lowlatency mode in VF.
    
    Solution:
    Set VFRAME_FLAG_GAME_MODE if low_latency_mode is equal to 7.
    
    Verify:
    None
    
    Change-Id: I118f55065b9b571261b1353a7a77749224551635
    Signed-off-by: yu.xie <yu.xie@amlogic.com>
    (cherry picked from commit 8e6f07e081f391ce96391c0b95a127c68099552a)

--- drivers/amvdec_ports/aml_vcodec_dec.c
+++ drivers/amvdec_ports/aml_vcodec_dec.c
@@ -1010,6 +1010,9 @@ static void post_frame_to_upper(struct aml_vcodec_ctx *ctx,
 			planes = aml_buf->planes_tw;
 	}
 
+	vf->flag |= (ctx->config.parm.dec.cfg.low_latency_mode == 7) ?
+		VFRAME_FLAG_GAME_MODE : 0;
+
 	v4l_dbg(ctx, V4L_DEBUG_CODEC_OUTPUT,
 		"OUT_BUFF (%s, st:%d, seq:%d, idx:%d) vb:(%d, %px), vf:(%d, %px), ts:%llu, flag: 0x%x "
 		"Y:(%lx, %u) C/U:(%lx, %u) V:(%lx, %u)\n",
