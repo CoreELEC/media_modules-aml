ed8bd6d6fb6e3b300665d338437406476440763c
kuan.hu <kuan.hu@amlogic.com>
2024-09-23 19:03:13 +0800

    vp9: CB1 fixed Resolution change & seek crash. [1/1]
    PD#TV-132120
    PD#SWPL-189310
    
    Problem:
    1.After BUFEMPTY, you will enter the buff scene and cycle
    high-frequency work, resulting in high CPU usage
    2.The subcontracting error sends two frames of different resolutions
    in one pack, resulting in abnormal resolution switching of one package
    of multi-frame logic processing
    3.An exception caused by the concurrency of resolution change and seek
    
    Solution:
    1.Optimize the BUFEMPTY logic in frame mode
    2.Compatible with one pack of multi-frame switching resolutionscenarios
    3.Compatible with resolution switching, seek concurrent scenarios
    
    Verify:
    T5M
    
    Change-Id: I36274d0a13719f168dd4b6070f177965253f8d28
    Signed-off-by: kuan.hu <kuan.hu@amlogic.com>
    Signed-off-by: gongwei.chen <gongwei.chen@amlogic.com>

--- drivers/frame_provider/decoder/utils/vdec.c
+++ drivers/frame_provider/decoder/utils/vdec.c
@@ -4247,6 +4247,7 @@ EXPORT_SYMBOL(vdec_reset);
 int vdec_v4l2_reset(struct vdec_s *vdec, int flag)
 {
 	if (flag != 2) {
+		vdec->reset_input_flag = true;
 		if (!vdec->disable_vfm) {
 			if (vdec->vframe_provider.name)
 				vf_unreg_provider(&vdec->vframe_provider);
@@ -4299,6 +4300,7 @@ int vdec_v4l2_reset(struct vdec_s *vdec, int flag)
 			}
 		}
 	} else {
+		vdec->reset_input_flag = false;
 		if (vdec->reset) {
 			vdec->reset(vdec);
 			if (vdec->slave)
