b57d23b92ae78fffc5e9f40e58344155bba69cfa
shihong.zheng <shihong.zheng@amlogic.com>
2024-11-19 16:21:59 +0800

    vav1: CB1 fix no memory alloc failed check. [1/1]
    PD#OTT-69997
    PD#SWPL-194572
    
    Problem:
    vav1 not return failed when alloc important buf failed.
    
    Solution:
    return invalid when alloc loopbuf failed.
    
    Verify:
    S928X
    
    Change-Id: I237b6cafda8f935f94f47df7a7fb66681e1e4e96
    Signed-off-by: shihong.zheng <shihong.zheng@amlogic.com>

--- drivers/frame_provider/decoder/vp9_fb/vvp9_fb.c
+++ drivers/frame_provider/decoder/vp9_fb/vvp9_fb.c
@@ -6073,7 +6073,7 @@ static void copy_loopbufs_ptr(buff_ptr_t* trg, buff_ptr_t* src)
 	trg->sys_imem_ptr_v = src->sys_imem_ptr_v;
 }
 
-static void init_fb_bufstate(struct VP9Decoder_s *pbi)
+static int init_fb_bufstate(struct VP9Decoder_s *pbi)
 {
 	int size = 0;
 	int ret;
@@ -6093,7 +6093,7 @@ static void init_fb_bufstate(struct VP9Decoder_s *pbi)
 	pbi->fb_buf_sys_imem.buf_start = tmp_phy_adr;
 	if (pbi->fb_buf_sys_imem_addr == NULL) {
 		pr_err("%s: failed to alloc fb_buf_sys_imem\n", __func__);
-		return;
+		return -ENOMEM;
 	}
 	memset(pbi->fb_buf_sys_imem_addr, 0, pbi->fb_buf_sys_imem.buf_size);
 	pbi->fb_buf_sys_imem.buf_end = pbi->fb_buf_sys_imem.buf_start + pbi->fb_buf_sys_imem.buf_size;
@@ -6111,7 +6111,7 @@ static void init_fb_bufstate(struct VP9Decoder_s *pbi)
 	pbi->fb_buf_mmu0.buf_start = tmp_phy_adr;
 	if (pbi->fb_buf_mmu0_addr == NULL) {
 		pr_err("%s: failed to alloc fb_mmu0_map\n", __func__);
-		return;
+		return -ENOMEM;
 	}
 	memset(pbi->fb_buf_mmu0_addr, 0, mmu_map_size);
 	pbi->fb_buf_mmu0.buf_size = mmu_map_size;
@@ -6124,7 +6124,7 @@ static void init_fb_bufstate(struct VP9Decoder_s *pbi)
 	pbi->fb_buf_mmu1.buf_start = tmp_phy_adr;
 	if (pbi->fb_buf_mmu1_addr == NULL) {
 		pr_err("%s: failed to alloc fb_mmu1_map\n", __func__);
-		return;
+		return -ENOMEM;
 	}
 	memset(pbi->fb_buf_mmu1_addr, 0, mmu_map_size);
 	pbi->fb_buf_mmu1.buf_size = mmu_map_size;
@@ -6137,7 +6137,7 @@ static void init_fb_bufstate(struct VP9Decoder_s *pbi)
 			pbi->fb_buf_mmu0_addr);
 	if (ret != 0) {
 		pr_err("%s: failed to alloc fb_mmu0 pages", __func__);
-		return;
+		return -ENOMEM;
 	}
 
 	ret = decoder_mmu_box_alloc_idx(
@@ -6147,7 +6147,7 @@ static void init_fb_bufstate(struct VP9Decoder_s *pbi)
 			pbi->fb_buf_mmu1_addr);
 	if (ret != 0) {
 		pr_err("%s: failed to alloc fb_mmu1 pages", __func__);
-		return;
+		return -ENOMEM;
 	}
 
 	pbi->fb_buf_lmem0.buf_size = IFBUF_LMEM0_SIZE * pbi->fb_ifbuf_num;
@@ -6196,6 +6196,7 @@ static void init_fb_bufstate(struct VP9Decoder_s *pbi)
 	pbi->bk.sys_imem_ptr = pbi->fb_buf_sys_imem.buf_start;
 	pbi->fr.sys_imem_ptr_v = pbi->fb_buf_sys_imem_addr;
 
+	return 0;
 }
 
 static void uninit_fb_bufstate(struct VP9Decoder_s* pbi)
@@ -10436,7 +10437,11 @@ static int vp9_local_init(struct VP9Decoder_s *pbi)
 		pbi->backend_decoded_count = 0;
 		pbi->fb_wr_pos = 0;
 		pbi->fb_rd_pos = 0;
-		init_fb_bufstate(pbi);
+		ret = init_fb_bufstate(pbi);
+		if (ret < 0) {
+			pr_info("init_fb_bufstate failed\n");
+			return ret;
+		}
 		copy_loopbufs_ptr(&pbi->next_bk[pbi->fb_wr_pos], &pbi->fr);
 	}
 #endif
