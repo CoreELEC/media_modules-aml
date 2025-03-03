a426f38e3ca9df14c3085b002de58b33de073149
xing.xu <xing.xu@amlogic.com>
2025-03-03 15:34:23 +0800

    h264: CB2 mbaff playback freezes [1/1]
    PD#SWPL-204389
    PD#IPTV-33010
    PD#IPTV-33012
    
    Problem:
    When getting a free buffer, check_force_interlace
    returns true, causing the decoder to get an extra
    buffer, which leads to a buffer shortage and eventually
    a stall.
    
    Solution:
    If the current stream is MBAFF or interlaced, do not execute
    
    Verify:
    gxlx4
    
    Change-Id: Ib39b8272e233d132fa90393b8749c6f58fc49e16
    Signed-off-by: xing.xu <xing.xu@amlogic.com>

--- drivers/frame_provider/decoder_v4l/h264_multi/vmh264.c
+++ drivers/frame_provider/decoder_v4l/h264_multi/vmh264.c
@@ -2594,14 +2594,15 @@ int v4l_get_free_buf_idx(struct vdec_s *vdec)
 				pic_struct = p_H264_Dpb->mVideo.dec_picture->pic_struct;
 				if (v4l->picinfo.field == V4L2_FIELD_INTERLACED || //frame_mbs_only_flag
 					pic_struct == PIC_TOP_BOT ||
-					pic_struct == PIC_BOT_TOP)
+					pic_struct == PIC_BOT_TOP) {
 					aml_buf_get_ref(&v4l->bm, aml_buf);
+				} else if (check_force_interlace(hw, hw->frame_width, hw->frame_height)) {
+					aml_buf_get_ref(&v4l->bm, hw->aml_buf);
+				}
 
 				if (pic_struct == PIC_TOP_BOT_TOP ||
 					pic_struct == PIC_BOT_TOP_BOT) {
 					aml_buf_get_ref(&v4l->bm, aml_buf);
-				} else if (check_force_interlace(hw, hw->frame_width, hw->frame_height)) {
-					aml_buf_get_ref(&v4l->bm, hw->aml_buf);
 				}
 			}
 
