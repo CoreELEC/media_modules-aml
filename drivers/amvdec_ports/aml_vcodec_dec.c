37d0c1d131139c46e545ef4c26f5d0aeaa7d3ba7
lele xiang <lele.xiang@amlogic.com>
2025-03-11 09:29:08 +0000

    V4L2: CB1 System has memory leak when play H264 interlace stream. [1/1]
    PD#SWPL-204594
    
    Problem:
    In the V4L2 delay_alloc , the size of dmabuf is not updated,
    leading to umap failure and the buffer not being released properly.
    
    Solution:
    update the size of dmabuf, when delay_alloc yuv buffer
    
    Verify:
    s7d
    
    Change-Id: Ic75d31ecec84d7fe42379f5752a93afe8a549b9c
    Signed-off-by: lele xiang <lele.xiang@amlogic.com>

--- drivers/amvdec_ports/aml_vcodec_dec.c
+++ drivers/amvdec_ports/aml_vcodec_dec.c
@@ -2150,7 +2150,7 @@ static int aml_uvm_buf_delay_alloc(struct aml_vcodec_ctx *ctx,
 				ctx->picinfo.y_len_sz + ctx->picinfo.c_len_sz);
 
 	mbuf->size =  ctx->picinfo.y_len_sz + ctx->picinfo.c_len_sz;
-
+	dbuf->size = mbuf->size;
 	handle = dbuf->priv;
 
 	if (ctx->master_buf) {
