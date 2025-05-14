56f30399e10253b96e7f556b75d6d5b354b1991f
Portisch <hugo.portisch@yahoo.de>
2025-05-14 12:16:39 +0200

    Revert "av1_fb: ensure enough memory to hold the need pages"
    This reverts commit 37fc909466d9d470cc513df39600833d3e4f2ebd.
    
    This commit do cause DMC violation on 8k media.

--- drivers/frame_provider/decoder/vav1_fb/av1_fb_hw.c
+++ drivers/frame_provider/decoder/vav1_fb/av1_fb_hw.c
@@ -643,7 +643,7 @@ static int init_fb_bufstate(struct AV1HW_s *hw)
 	dma_addr_t tmp_phy_adr;
 	int mmu_4k_number = hw->fb_ifbuf_num * av1_mmu_page_num(hw, hw->max_pic_w,
 			hw->max_pic_h, 1);
-	int mmu_map_size = (((mmu_4k_number * 4) >> 6) + 1) << 6;
+	int mmu_map_size = ((mmu_4k_number * 4) >> 6) << 6;
 	int tvp_flag = vdec_secure(hw_to_vdec(hw)) ?
 		CODEC_MM_FLAGS_TVP : 0;
 
