b9c86220aceaa770d1191eac589aaba6aa95724a
Portisch <hugo.portisch@yahoo.de>
2025-09-12 07:48:38 +0200

    decoders: change double write mode to 1/4:1/4 Mode is need for screen capture.

--- drivers/frame_provider/decoder/vp9_fb/vvp9_fb.c
+++ drivers/frame_provider/decoder/vp9_fb/vvp9_fb.c
@@ -235,7 +235,7 @@ static u32 efficiency_mode = 0;
  * 0x300, if > 720p, use mode 4, else use mode 1;
  * 0x10000, double write p010 enable
  */
-static u32 double_write_mode;
+static u32 double_write_mode = 0x3;
 
 /* triple_write_mode:
  * 0, no triple write;
